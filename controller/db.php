3ea313017230d48f61e68f211566252c0ced969a
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-07-07 19:13:21 +0300

    created Singleton class DB to create new instance of PDO class if not already instantiated.

---
+++ controller/db.php
@@ -0,0 +1,43 @@
+<?php
+
+
+     class DB {
+
+        private static $writeDBConnection;
+        private static $readDBConnection;
+
+        private function __construct()
+        {
+        }
+
+        public static function connectWriteDB()
+        {
+            if(self::$writeDBConnection === null)
+            {
+                self::$writeDBConnection = new PDO('mysql:dbname=taskssdb;host=localhost;charset=utf8', 'root','');
+                self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+            }
+
+            return self::$writeDBConnection;
+
+        }
+
+        public static function connectReadDB()
+        {
+            if(self::$readDBConnection === null)
+            {
+                self::$readDBConnection = new PDO('mysql:dbname=tasksdb;host=localhost;charset=utf8', 'root','');
+                self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+            }
+
+            return self::$readDBConnection;
+
+        }
+
+
+    }
+
+
+
